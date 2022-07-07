15e24fda09ffac44a6b6c2cae5b6ede191e399d0
blakef1985 <blakef1985@gmail.com>
2022-07-07 19:55:33 +0000

    test readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 4205
 4205
 this is a leader board app
+WEB_APP
+
++-- templates
+|-- base.html
+|-- index.html
+  +-- player.html
--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+#WEB_APP
+
+#+-- app.py 
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
    return "<p>Hello, World!</p>"
+#WEB_APP
+
+#+-- templates
+#|-- base.html
+#|-- index.html
+ # +-- player.html
